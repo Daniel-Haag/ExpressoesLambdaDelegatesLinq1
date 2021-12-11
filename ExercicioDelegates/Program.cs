4b16257bea74991c19bf97b6f3d9904898277d5c
Daniel Haag <danni.haag91@gmail.com>
2021-12-10 22:36:44 -0300

    primeiro commit

---
+++ ExercicioDelegates/Program.cs
@@ -0,0 +1,21 @@
+﻿using ExercicioDelegates.Servicos;
+using System;
+
+namespace ExercicioDelegates
+{
+    class Program
+    {
+        delegate double BinaryCalculatorOperation(double num1, double num2);
+        static void Main(string[] args)
+        {
+            double a = 5;
+            double b = 5;
+
+            BinaryCalculatorOperation op = Calculos.Soma;
+
+            double result = op(a, b);
+
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ ExercicioDelegates/Servicos/Calculos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExercicioDelegates.Servicos
+{
+    class Calculos
+    {
+        public static double Soma(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public static double Subtracao(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public static double Multiplicacao(double num1, double num2, double num3)
+        {
+            return num1 * num2 * num3;
+        }
+    }
+}
